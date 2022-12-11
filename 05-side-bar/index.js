2e076a59ef9effef8af892b7be8c0206e48facd1
lucianoscabora <lucianoscabora@gmail.com>
2022-12-10 22:59:12 -0300

    sidebar toggle JS - finished

---
+++ 05-side-bar/index.js
@@ -0,0 +1,24 @@
+// select elements
+
+const toggleBtn = document.querySelector('.sidebar-toggle');
+const closeBtn = document.querySelector('.close-btn');
+const sidebar = document.querySelector('.sidebar');
+
+toggleBtn.addEventListener('click', function() {
+    console.log(sidebar.classList);
+   /*  if(sidebar.classList.contains('show-sidebar')) {
+        sidebar.classList.remove('show-sidebar');
+    }
+    else {
+        sidebar.classList.add('show-sidebar');
+    } */
+
+    // using the toggle method
+
+    sidebar.classList.toggle('show-sidebar');
+});
+
+
+closeBtn.addEventListener('click', function() {
+    sidebar.classList.remove('show-sidebar');
+})
